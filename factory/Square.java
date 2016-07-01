effdd929069e0f54c2f7dda2d5843768012936f9
yxiangling <yxiangling@hotmail.com>
2016-07-01 11:43:40 +0800

    工厂模式

---
+++ factory/Square.java
@@ -0,0 +1,10 @@
+package factory;
+import factory.Shape;
+
+public class Square implements Shape {
+
+	@Override
+	public void draw() {
+	      System.out.println("Inside Square::draw() method.");
+	}
+}
